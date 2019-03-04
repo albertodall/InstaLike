1473f21bbdd9f4634c18c25b6606e7501fdd2b07
Alberto Dallagiacoma <alberto.dallagiacoma@live.it>
2019-03-04 17:28:20 +0100

    Added notifications view and related query.

---
+++ InstaLike.Web/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaLike.Web.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly IMediator _dispatcher;
+
+        public NotificationController(IMediator dispatcher)
+        {
+            _dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Count()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ InstaLike.Web/Data/Query/NotificationsQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using InstaLike.Web.Models;
+using MediatR;
+using NHibernate;
+
+namespace InstaLike.Web.Data.Query
+{
+    public class NotificationsQuery : IRequest<NotificationModel[]>
+    {
+        public int UserID { get; }
+        public bool IncludeReadNotifications { get; }
+
+        public NotificationsQuery(int userID, bool includeReadNotifications)
+        {
+            UserID = UserID;
+            IncludeReadNotifications = includeReadNotifications;
+        }
+    }
+
+    internal class NotificationsQueryHandler : IRequestHandler<NotificationsQuery, NotificationModel[]>
+    {
+        private readonly ISession _session;
+
+        public NotificationsQueryHandler(ISession session)
+        {
+            _session = session ?? throw new ArgumentNullException(nameof(session));
+        }
+
+        public async Task<NotificationModel[]> Handle(NotificationsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
