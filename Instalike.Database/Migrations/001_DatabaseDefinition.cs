35c26bf628737af73ecee8f615ad82035ab3445a
Alberto Dallagiacoma <alberto.dallagiacoma@live.it>
2019-06-10 23:00:59 +0200

    Added project for database migrations based on FluentMigrator

---
+++ Instalike.Database/Migrations/001_DatabaseDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentMigrator;
+
+namespace Instalike.Database.Migrations
+{
+    public class DatabaseDefinition : Migration
+    {
+        public override void Up()
+        {
+            Execute.Script(@"Scripts\DatabaseDefinition.sql");
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DROP DATABASE InstaLike");
+        }
+    }
+}
