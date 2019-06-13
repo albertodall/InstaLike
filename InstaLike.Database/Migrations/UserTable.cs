497e395b198c12841cc9cd1b6a1a61b89bc8fd21
Alberto Dallagiacoma <alberto.dallagiacoma@live.it>
2019-06-13 15:24:57 +0200

    User table creation using fluent syntax

---
+++ InstaLike.Database/Migrations/UserTable.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+using FluentMigrator.SqlServer;
+
+namespace Instalike.Database.Migrations
+{
+    [Migration(1, "Create 'User' Table in schema 'dbo'")]
+    public class UserTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("User").InSchema("dbo")
+                .WithColumn("ID").AsInt32().Identity(1, 1);
+
+            Create.Column("Nickname").OnTable("User").InSchema("dbo").AsString(20).NotNullable().WithDefaultValue(string.Empty);
+            Create.Column("Password").OnTable("User").InSchema("dbo").AsString(64).NotNullable().WithDefaultValue(string.Empty);
+            Create.Column("Name").OnTable("User").InSchema("dbo").AsString(30).NotNullable().WithDefaultValue(string.Empty);
+            Create.Column("Surname").OnTable("User").InSchema("dbo").AsString(50).NotNullable().WithDefaultValue(string.Empty);
+            Create.Column("Email").OnTable("User").InSchema("dbo").AsString(50).NotNullable().WithDefaultValue(string.Empty);
+            Create.Column("Biography").OnTable("User").InSchema("dbo").AsString(500).NotNullable().WithDefaultValue(string.Empty);
+            Create.Column("RegistrationDate").OnTable("User").InSchema("dbo").AsDateTimeOffset().NotNullable().WithDefault(SystemMethods.CurrentDateTimeOffset);
+
+            Create.PrimaryKey("PK_User").OnTable("User").WithSchema("dbo").Column("ID").Clustered();
+
+            Create.Index("IX_User_Credentials").OnTable("User").InSchema("dbo")
+                .OnColumn("Nickname").Ascending()
+                .OnColumn("Password").Ascending();
+
+            Create.UniqueConstraint("IX_User_Nickname").OnTable("User").WithSchema("dbo").Column("Nickname").NonClustered();
+
+            // ProfilePicture varbinary(max)
+            // ProfilePictureGuid ROWGUIDCOL (default newsequentiald)
+            // Unique Key ProfilePictureGuid
+        }
+    }
+}
